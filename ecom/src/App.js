1d742944fe6e6a05e75ce63d10b0d946117b160e
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-06-29 17:27:45 +0530

    .

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -2,7 +2,6 @@ import { React, Suspense, lazy, useRef } from "react";
 import MaterialUi from "./Components/MaterialUi";
 import MuiBtn from "./Components/MuiBtn";
 import MuiGrid from "./MuiGrid";
-import MuiStack from "./MuiStack";
 import Usereducer from "./Components/UseCallback";
 import { Route, Routes } from "react-router-dom";
 import User from "./Components/User";
@@ -15,6 +14,8 @@ import Effect from "./Components/Effect";
 import Callback from "./Components/callback";
 import Iife from "./Components/Iife";
 import Time from "./Components/Time";
+import { createContext, useContext } from "react";
+
 const Lazy = lazy(() => import("./Components/Lazy"));
 
 function App() {
@@ -22,6 +23,30 @@ function App() {
   function updateInput() {
     inputval.current.value = "1000";
   }
+
+const Context = createContext();
+
+const Child5 = () => {
+  const context = useContext(Context);
+  return <div>{context.data}</div>;
+};
+
+const Child4 = () => {
+  return <Child5 />;
+};
+
+const Child3 = () => {
+  return <Child4 />;
+};
+
+const Child2 = () => {
+  return <Child3 />;
+};
+
+const Child = () => {
+  return <Child2 />;
+};
+
   return (
     <div className="App">
       <Routes>
@@ -49,6 +74,10 @@ function App() {
       <Callback/>
       <Iife/>
       <Time/>
+
+      <Context.Provider value={{ data: "Data from context!" }}>
+      <Child />
+    </Context.Provider>
     </div>
   );
 }
